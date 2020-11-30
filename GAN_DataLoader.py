3e2f3a8b25f065bac04632396ccd22fddf8c4dc9
Esther <ykesther10@gmail.com>
2020-11-30 00:38:39 -0500

    Update GAN_DataLoader.py

--- GAN_DataLoader.py
+++ GAN_DataLoader.py
@@ -15,39 +15,40 @@ import torchvision.utils as vutils
 def dataloader(root = "\clean_dataset\train_data",
                image_size = 224,
                num_channels = 3,
-               batch_size = 4,
-               mean = None,
-               std = None):
+               batch_size = 4):
+#                mean = None,
+#                std = None):
 
-    if (mean == None or std == None):
-        normset = torchvision.datasets.ImageFolder(root=root, transform=transforms.Compose([transforms.ToTensor()]))
-        normloader = torch.utils.data.DataLoader(normset, batch_size=4, shuffle=True, num_workers=2)
+#     if (mean == None or std == None):
+#         normset = torchvision.datasets.ImageFolder(root=root, transform=transforms.Compose([transforms.ToTensor()]))
+#         normloader = torch.utils.data.DataLoader(normset, batch_size=4, shuffle=True, num_workers=2)
 
-        num_images = 0
-        mean = [0, 0, 0]
-        std = [0, 0, 0]
-        for inputs, _ in normloader:
-            num_images = num_images + 1
-            for i in range(3):
-                mean[i] += inputs[:, i, :, :].mean()
-                std[i] += inputs[:, i, :, :].std()
-        for i in range(3):
-            mean[i] = mean[i] / num_images
-            std[i] = std[i] / num_images
-        print(mean)
-        print(std)
+#         num_images = 0
+#         mean = [0, 0, 0]
+#         std = [0, 0, 0]
+#         for inputs, _ in normloader:
+#             num_images = num_images + 1
+#             for i in range(3):
+#                 mean[i] += inputs[:, i, :, :].mean()
+#                 std[i] += inputs[:, i, :, :].std()
+#         for i in range(3):
+#             mean[i] = mean[i] / num_images
+#             std[i] = std[i] / num_images
+#         print(mean)
+#         print(std)
 
     # transform = transforms.Compose([transforms.Normalize((mean[0], mean[1], mean[2]), (std[0], std[1], std[2]))],
     #                                transforms.Resize(image_size),
     #                                transforms.RandomRotation(45),
     #                                transforms.ToTensor())
     transform = transforms.Compose([
-        transforms.Resize((224, 224)),
+        transforms.Resize((image_size, image_size)),
+        transforms.RandomRotation(45),
         transforms.ToTensor()
     ])
 
     image_data = dset.ImageFolder(root=root,
-                               transform=transform)
+                                  transform=transform)
 
     dataset = torch.utils.data.TensorDataset(image_data, image_data)
 
